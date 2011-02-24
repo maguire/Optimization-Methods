9040685aae38cf71894fbf09ad04398c8c473bc9
Patrick <mcqlax31@gmail.com>
2011-02-24 00:42:35 -0500

    updated input of Grid from file

--- chemag-3a/chemag-3a/Grid.cpp
+++ chemag-3a/chemag-3a/Grid.cpp
@@ -11,52 +11,25 @@
 #include <stdlib.h>
 #include "Grid.h"
 
-vector<string> tokenize(const string &str, char delimit = ' ')
-// given a string split it into tokens by the given delimiter
-// this is a global function
-{
-    vector<string> tokens;
-    int prev = 0;
-
-    for (unsigned int i = 0; i < str.length(); i++)
-    {
-        if (str[i] == delimit)
-        {
-            tokens.push_back(str.substr(prev, i - prev));
-            prev = i + 1; // skip the delimiter
-        }
-    }
-
-    // the loop will skip the last token make sure that gets in the vector
-    tokens.push_back(str.substr(prev, str.length() - prev));
-
-    return tokens;
-}
-
 Grid::Grid(string in_fd)
 // public Grid constructor
 // given a file, read the file and create a matrix according to the file
 {
-    string line;
     ifstream fins;
     fins.open(in_fd.c_str());
-    string s;
 
     if (fins.is_open())
     {
-        //read the first line, which tells us the size of the matrix
-        getline(fins, line);
-        vector<string> vs = tokenize(line);
-        _size = atoi(vs[0].c_str());
-        mx = matrix<string>(atoi(vs[0].c_str()), atoi(vs[1].c_str()));
+        string sizex, sizey;
+        fins >> sizex >> sizey;
+        _size = atoi(sizex.c_str());
+        mx = matrix<string>(atoi(sizex.c_str()), atoi(sizey.c_str()));
         
         for (int i = 0; i < _size; i++)
         {
-            getline(fins, line);
-            vs = tokenize(line);
             for (int j = 0; j < _size; j++)
             {
-                mx[i][j] = vs[j];
+                fins>>mx[i][j];
             }
         }
     }
